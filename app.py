6b0f659cf0af53013c494d2da37e387d488e02c8
Badhan-alsoltech <badhan.alsoltech.astw70031@gmail.com>
2025-11-12 12:25:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -311,11 +311,6 @@ def external_images(filename):
 
 
 # === Auto-open browser ===
-def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000/")
-
-
 if __name__ == "__main__":
-    threading.Timer(1, open_browser).start()
-    print("🚀 Running on http://127.0.0.1:5000/")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
